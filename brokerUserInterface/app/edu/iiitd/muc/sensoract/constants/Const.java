1ed83654bec352e080f50ebc781385c9b6d667b9
Nipun <nipunb@iiitd.ac.in>
2012-05-18 18:04:22 +0530

    Fixed bug ..signup wasn't redirecting

--- brokerUserInterface/app/edu/iiitd/muc/sensoract/constants/Const.java
+++ brokerUserInterface/app/edu/iiitd/muc/sensoract/constants/Const.java
@@ -101,7 +101,7 @@ public class Const {
 	 * URL's
 	 */
 	public static final String URL_UI_SERVER = "http://localhost:9003/";
-	public static final String URL_BROKER_SERVER = "http://localhost:9001/";
+	public static final String URL_BROKER_SERVER = "http://192.168.1.174:9001/";
 	public static final String URL_BROKER_REGISTER_USER = URL_BROKER_SERVER
 			+ "user/register";
 	public static final String URL_BROKER_LOGIN_USER = URL_BROKER_SERVER
