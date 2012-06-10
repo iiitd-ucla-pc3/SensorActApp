962662b661d9c773086b870dc9ed2e6e124e4ec6
Nipun <nipunb@iiitd.ac.in>
2012-06-10 15:49:13 +0530

    Made enhancements to the UI

--- repositoryUserInterface/public/javascripts/application.js
+++ repositoryUserInterface/public/javascripts/application.js
@@ -1,9 +1,10 @@
-$(".forms").hide();
+/*$(".forms").hide();
 $("ul li a").click(function(){
 	var idToShow = $(this).attr("href");
 	$(idToShow).show().siblings(".forms").hide();
 	return false;
 });
+*/
 $("#sign_up_confirmation").keyup(function(){
 	if( $(this).val() != $("#sign_up_password").val() ) {
 		$(this).addClass("error").next().text("Passwords don't match.");
